87a55d7b64bbf6ef9d9ed3ca24bff4b37d4850ce
skyler <konata.izumi20@gmail.com>
2022-06-18 12:54:03 -0400

    fix metadata info

--- js/SongParser.js
+++ js/SongParser.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         AMQ Song Difficulty Counter
-// @namespace    https://github.com/TheJoseph98
-// @version      1.3.0
-// @description  Counts the songs by individual difficulty, per song type
-// @author       TheJoseph98
+// @name         AMQ Song Parser
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Parses all songs in a list.
+// @author       umi feet
 // @grant        GM_xmlhttpRequest
 // @connect      script.google.com
 // @connect      script.googleusercontent.com
 // @match        https://animemusicquiz.com/*
-// @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
-// @updateURL    https://github.com/TheJoseph98/AMQ-Scripts/raw/master/amqSongDifficultyCounter.user.js
+// @updateURL    https://github.com/bot15498/amq-player/blob/master/js/SongParser.js
+// @downloadURL  https://github.com/bot15498/amq-player/blob/master/js/SongParser.js
 // ==/UserScript==
